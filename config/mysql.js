1610c89f3fa9607cf8f9f6cf34aa8181feec8474
jimdev90 <jimpv100190@gmail.com>
2023-12-20 20:52:04 -0500

    cambiando de respositorio

---
+++ config/mysql.js
@@ -0,0 +1,6 @@
+const { Sequelize } = require("sequelize");
+
+const database = process.env.MYSQL_DATABASE;
+const username = process.env.MYSQL_USER;
+const password = process.env.MYSQL_PASSWORD;
+const host = process.env.MYSQL_HOST;
